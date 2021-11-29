eb53c6c7a131d619c931491c027d85a4644784f8
Julio Cervantes <65304872+JCervantesB@users.noreply.github.com>
2021-11-29 14:05:25 -0700

    Añadidas mas rutas
    Se añadieron rutas a Categorías, Marcas y Departamentos

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,47 +7,50 @@ import json
 
 class EquiposApi(http.Controller):
 
-    @http.route('/api/equipos', auth='public', method=['GET'], csrf=False)
+    @http.route('/api/equipos', auth='public', method=['GET'], csrf=False, cors='*')
     def get_equipos(self, **kw):
         try:
             equipos = http.request.env['maintenance.equipment'].sudo().search_read(
                 [], 
-                ['id', 'code', 'name', 'category_id'],
-                offset=0,
-                limit=50
+                ['id', 'code', 'name', 'category_id', 'employee_id', 'department_id', 'x_studio_marca', 'model', 'serial_no', 'x_studio_ubicacion', 'x_studio_selection_field_shlje']
                 )
             res = json.dumps(equipos, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
             return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
 
-    @http.route('/api/equipos/<id>', auth='public', method=['GET'], csrf=False)
-    def get_equipo_id(self, id, **kw):
+    @http.route('/api/equipos/marcas', auth='public', method=['GET'], csrf=False, cors='*')
+    def get_marcas(self, **kw):
         try:
-            rec = http.request.env['maintenance.equipment'].sudo().search([('id', '=', int(id))], limit=1)
-            equipo = {
-                'responsable': {
-                    'empleado': rec.employee_id.name,
-                    'puesto': rec.x_studio_puesto,
-                    'departamento': rec.department_id.name,
-                    'fecha_asignacion': rec.assign_date,
-                },
-                'equipo': {
-                    'codigo': rec.code,
-                    'nombre': rec.name,
-                    'categoria': rec.category_id.name,
-                    'marca': rec.x_studio_marca.name,
-                    'modelo': rec.model,
-                    'numero_serie': rec.serial_no,
-                },
-                'caracteristicas': {
-                    'procesador': rec.x_studio_procesador,
-                    'memoria': rec.x_studio_memoria,
-                    'almacenamiento': rec.x_studio_almacenamiento,
-                    'garantia': rec.warranty_date,
-                }
-            }
-            res = json.dumps(equipo, ensure_ascii=False).encode('utf-8')
+            marcas = http.request.env['product.brand'].sudo().search_read(
+                [], 
+                ['id', 'name']
+                )
+            res = json.dumps(marcas, ensure_ascii=False).encode('utf-8')
+            return Response(res, content_type='application/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
+
+    @http.route('/api/equipos/categorias', auth='public', method=['GET'], csrf=False, cors='*')
+    def get_categorias(self, **kw):
+        try:
+            categorias = http.request.env['maintenance.equipment.category'].sudo().search_read(
+                [], 
+                ['id', 'name']
+                )
+            res = json.dumps(categorias, ensure_ascii=False).encode('utf-8')
+            return Response(res, content_type='application/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
+
+    @http.route('/api/departamentos', auth='public', method=['GET'], csrf=False, cors='*')
+    def get_departamentos(self, **kw):
+        try:
+            departamentos = http.request.env['hr.department'].sudo().search_read(
+                [], 
+                ['id', 'name']
+                )
+            res = json.dumps(departamentos, ensure_ascii=False).encode('utf-8')
             return Response(res, content_type='application/json;charset=utf-8', status=200)
         except Exception as e:
             return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
